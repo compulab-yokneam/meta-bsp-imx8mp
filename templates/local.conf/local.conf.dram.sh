8ab95a891d9f08b566841a589b63ec7c1f52cdc7
Valentin Raevsky <valentin@compulab.co.il>
2026-06-10 23:58:03 +0300

    Update meta-layer files for u-boot-compulab to build flash.bin
    Signed-off-by: Valentin Raevsky <valentin@compulab.co.il>

--- templates/local.conf/local.conf.dram.sh
+++ templates/local.conf/local.conf.dram.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-declare -A dram_configs=( [D1]='d1d8' [D2]='d2' [D4]='d4' [D8]='d1d8' )
+declare -A dram_configs=( [D1]='d1' [D2]='d2' [D4]='d4' [D8]='d8' [D1D2]='d1d2' [D1D4]='d1d4' [D1D8]='d1d8' [D2D4]='d2d4' [D2D8]='d2d8' [D4D8]='d4d8' )
 
 _get_dram_config() {
     local select_string=${!dram_configs[@]}" <<"
@@ -22,7 +22,7 @@ get_dram_config() {
     DRAM_CONF=${dram_configs[${DRAM:-"empty"}]:-$(_get_dram_config)}
     DRAM_CONF=${DRAM_CONF:-"d4"}
 cat << eof
-DRAM_CONF = "${DRAM_CONF}"
+DRAM_CONF ?= "${DRAM_CONF}"
 eof
 }
 
